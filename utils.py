87476066b32c6351d7749ca042ea614e4ef5ae01
Nicolas Farrugia <nicolas.farrugia@imt-atlantique.fr>
2022-09-29 14:52:48 +0200

    kept only necessary function

--- utils.py
+++ utils.py
@@ -2,31 +2,22 @@
 import os
 from tqdm import tqdm
 #Preprocessing audio folder
+import numpy as np 
+#add relative
 
-def audiopath_resample(audiopath,outputpath = 'audio22k' ,sr = 22050):
-    """This method changes the sampling rate and the number of channels for each .wav file in the audiopath
-    """
-    resultdir = os.path.join(outputpath)
-    os.makedirs(resultdir,exist_ok=True)
-    listwav = []
-    for c in os.listdir(audiopath):
-        if c[-3:] == 'wav':
-            listwav.append(c)           
     
-    if(len(listwav) == 0):
-        print('There are not wav files in :',audiopath)
-        return 
-    
-    for c in tqdm(listwav):
-        command = "sox {} -r {} -c 1 {}/{}_.wav".format(audiopath+c,sr,outputpath,c[:-4])
-        print(command)
-        if (os.system(command) != 0): return                
-    print("Audio saved in ",resultdir)
-    
-def audio_resample(audiopath ,sr = 22050):
-    command = "sox {} -r {} -c 1 {}/{}_.wav".format(audiopath,sr,outputpath,c[:-4])
-    print(command)
-    if (os.system(command) != 0): return     
+def vector_to_scenes(scenes_vector):
+    f = open("sound/categories_places2.txt",'r')
+    categories = f.read().split("\n")
+    for example in range((scenes_vector.shape[0])):
+        for x in range((scenes_vector.shape[2])):
+            index = np.argmax(scenes_vector[example,:,x,0])
+            print(categories[index])
 
-        
-    
+def vector_to_obj(obj):
+    f = open("sound/objects.txt",'r')
+    categories = f.read().split("\n")
+    for example in range(obj.shape[0]):
+        for x in range(obj.shape[2]):
+            index = np.argmax(obj[example,:,x,0])
+            print(categories[index])
